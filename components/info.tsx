de0819c9185fc40380d19f0677989b7a3666fe22
Ida Sofie Grude <sofie.grude@gmail.com>
2024-03-29 20:13:39 +0100

    First version

--- components/info.tsx
+++ components/info.tsx
@@ -1,5 +1,21 @@
 import Image from "next/image";
 
 export default function Info() {
-  return <h1>Hei info</h1>;
+  return (
+    <section className="infobox">
+      <div>
+        <Image
+          alt="profile picture"
+          src="/solar.png"
+          width={300}
+          height={300}
+        />
+      </div>
+      <div className="pl-10">
+        <h2 className="text-3xl">Utvikler</h2>
+        <h1 className="text-6xl font-bold tracking-tight"> Ida Sofie Grude</h1>
+        <h3 className="text-xl">post@idasofie.dev</h3>
+      </div>
+    </section>
+  );
 }
